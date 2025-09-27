9347ce4d21d71f3e558bba8e66b6d9a3d76d75ba
mcasals <mcasals@fie.undef.edu.ar>
2025-09-27 17:02:57 -0300

    subo mi presentacion

---
+++ practica/presentacion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Presentación básica
+    printf("Hola, mi nombre es Mauricio Casals.\n");
+    printf("Soy docente y desarrollador de software.\n");
+    printf("Actualmente trabajo con C, Python, Django y ROS2.\n");
+    printf("Este es mi primer programa en C a modo de presentacion.\n");
+
+    return 0;
+}
