6b3c5d30d13a98029ce54cbed40bf0102a6973d9
Florencia <florenciagerstner1@gmail.com>
2025-10-01 13:33:31 -0300

    Agrego presentacion en c

---
+++ PresentacionFG.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hola, soy Florencia Gerstner.\n");
+    printf("Esta es una breve presentación en C.\n");
+    printf("Soy estudiante\n");
+    return 0;
+}
