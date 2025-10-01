21ccc489acd9f0570ba2225280e08b9a62ca5b60
Alejandro Gerstner <alejandrogerstner@gmail.com>
2025-10-01 14:15:43 -0300

    Agrego mi programa de presentación en C

---
+++ presentacionalejandrogerstner.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Presentación simple
+    printf("Hola, mi nombre es Alejandro Gerstner.\n");
+    printf("Estoy aprendiendo a programar en C.\n");
+    printf("¡Mucho gusto!\n");
+
+    return 0;
+}
