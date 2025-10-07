55d3f1cc37c762c2e203369fbc250e79cd00b0e0
Alejandro Gerstner <alejandrogerstner@gmail.com>
2025-10-07 00:53:30 -0300

    nueva presentacion

---
+++ presentacion2alejgerstner.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void presentacion(){
+    char nombre[] = "ALEJANDRO GERSTNER";
+    printf("\nMi nombre es %s  \n", nombre );
+    printf("esta es la segunda presentacion\n");
+    printf("\n");
+    
+    
+
+}
+
+int main(){
+    presentacion();
+    return(0);
+}
+
