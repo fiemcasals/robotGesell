ea0760b3df55f5697cdaddea7c24a22bea20cf7d
Florencia <florenciagerstner1@gmail.com>
2025-10-08 09:52:19 -0300

    Presentacion 2 en C, utilizando "char"

---
+++ Presentacion2FG.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char nombre[] = "Florencia";
+    char apellido[] = "Gerstner";
+
+    printf("Mi nombre es %s %s.\n", nombre, apellido);
+    printf("Esta es mi segunda presentacion en C.");
+
+    return 0;
+}
