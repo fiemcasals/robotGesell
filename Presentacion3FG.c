26632de7f00ed4ed49d066b139cba089711cefdb
Florencia <florenciagerstner1@gmail.com>
2025-10-15 12:31:36 -0300

    Presentacion 3 en C, char

---
+++ Presentacion3FG.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char nombre [100];
+
+    printf("Ingresar nombre y apellido: ");
+
+    //Lee desde la entrada estandar y lo guarda, sizeof asegura el limite de caracrteres.
+    if(fgets(nombre, sizeof(nombre) stdin) != NULL);
+
+    //Eliminar salto de linea
+    char *p = strchr(nombre, '\n'); //Busca el salto de linea, el enter.
+    if (p) *p = '\0'; //Lo reemplaza por un caracter nulo.
+
+    printf("Hola %s\n", nombre);
+
+
+
+}
