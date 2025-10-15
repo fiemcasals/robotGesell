ec335a74ac2dfe49b56a1e0cfe33680e915b10d7
Florencia <florenciagerstner1@gmail.com>
2025-10-15 12:50:49 -0300

    Presentacion 3 en C, CORRECCION: "," entes de stdin, "{ }" despues del if

--- Presentacion3FG.c
+++ Presentacion3FG.c
@@ -7,14 +7,14 @@ int main(){
     printf("Ingresar nombre y apellido: ");
 
     //Lee desde la entrada estandar y lo guarda, sizeof asegura el limite de caracrteres.
-    if(fgets(nombre, sizeof(nombre), stdin) != NULL)
+    if(fgets(nombre, sizeof(nombre), stdin) != NULL){
+        //Eliminar salto de linea
+        char *p = strchr(nombre, '\n'); //Busca el salto de linea, el enter.
+        if (p) *p = '\0'; //Lo reemplaza por un caracter nulo.
+        
+        printf("Hola %s\n", nombre);
+    }
 
-    //Eliminar salto de linea
-    char *p = strchr(nombre, '\n'); //Busca el salto de linea, el enter.
-    if (p) *p = '\0'; //Lo reemplaza por un caracter nulo.
-
-    printf("Hola %s\n", nombre);
-
-
+    return 0;
 
 }
