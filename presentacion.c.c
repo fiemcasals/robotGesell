a396585c7255ebc24b9824ce1eecfae60604656c
TuNombre <tuemail@example.com>
2025-10-28 00:12:29 -0300

    Estoy seguro

---
+++ presentacion.c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hola, mi nombre es Cristian Avalo.\n");
+    printf("Soy docente en escuelas secundarias.\n");
+    printf("Actualmente trabajo en Villa Gesell.\n");
+    printf("Este es mi primer programa en C a modo de presentacion.\n");
+
+    return 0;
+}
